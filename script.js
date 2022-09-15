a0e2d06c2b86896d783b62f20cd9ae95ddb16567
Chris <christopher.soncrant@gmail.com>
2022-09-14 19:30:23 -0700

    added draft for data structure

--- script.js
+++ script.js
@@ -1,17 +1,138 @@
+//chapters (array)
+    //chapter (object)
+        //title
+        //verses (array)
+            //verse (array)
+                //paragraphs
+        //reflection (object)
+            //title
+            //date
+            //image (object)
+                //src
+                //altTag
+            //paragraphs (array)
+                //paragraph
 
-const chapters = {
-    chapterOne: {
+// const chapters = [
+//     {
+//         chapter: {
+//             verses: [
+//                 "line1", "line2",
+//             ],
+//         },
+//         reflection: {
+//             title: "str",
+//             date: "str",
+//             image: {
+//                 src: "str",
+//                 altTag: "str",
+//             },
+//             writings: ["str1", "str2",],
+//         }
+//     },
+// ];
 
-    }
+const chaptersData = [
+    {
+        title: "str",
+        verses: [
+            "line1", "line2",
+        ],
+        reflection: {
+            title: "str",
+            date: "str",
+            image: {
+                src: "str",
+                altTag: "str",
+            },
+            writings: ["str1", "str2",],
+        },
+    },
+    {
+        title: "str",
+        verses: [
+            "line1", "line2",
+        ],
+        reflection: {
+            title: "str",
+            date: "str",
+            image: {
+                src: "str",
+                altTag: "str",
+            },
+            writings: ["str1", "str2",],
+        },
+    },
+    {
+        title: "str",
+        verses: [
+            ["str1", "str2",], 
+            ["str1", "str2",], 
+        ],
+        reflection: {
+            title: "str",
+            date: "str",
+            image: {
+                src: "str",
+                altTag: "str",
+            },
+            writings: ["str1", "str2",],
+        },
+    },
+];
+
+//input: string - output: string
+function createLine(line) {
+    return `<p>${line}</p>`;
 }
+
+//input: verse array - output: string
+function createVerse(lines) {
+    return lines.map(function(line) {
+        return createLine(line);
+    }).join("");
+}
+
+function createVerses(verses) {
+    
+    let template = `<ol>`;
+
+    verses.forEach(function(verse) {
+
+        template += `<li>${verse}</li>`;
+
+    });
+
+    template += `</ol>`;
+
+    return template;
+}
+
+const output = document.querySelector('output');
+
+output.innerHTML = createVerses(chaptersData[0].verses);
+
+function createPoem(verses) {
+    
+    return `
+        <div class="poem">
+        </div>  
+    `
+}
+
+function renderVerse(verse) {
+
+}
+
+console.log(str);
 
 const chapterOne = {
     verses: "The tao that can be told\nis not the eternal Tao\nThe name that can be named\nis not the eternal Name.\n\nThe unnamable is the eternally real.\nNaming is the origin\nof all particular things."
-}
+};
 
 let strArr = [
-    ["The tao that can be told","is not the eternal Tao","The name that can be named","is not the eternal Name."],
-    ["The unnamable is the eternally real.","Naming is the origin","of all particular things."]
+    ["The tao that can be told", "is not the eternal Tao", "The name that can be named", "is not the eternal Name."],
+    ["The unnamable is the eternally real.", "Naming is the origin", "of all particular things."]
 ]
 
 //This takes in an array of strings; each string is one line of one verse; what is returned is a string consisting of all the strings in the input array joined together with the new line character. This string is one verse and is ready to be added into html. Actually I don't think this is needed. 
@@ -24,6 +145,3 @@ function addNewLineCharacter(strArr) {
 function addVerses(arr) {
     return arr.map(addNewLineCharacter)
 }
-
-
-
